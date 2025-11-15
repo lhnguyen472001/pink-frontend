49fdbd9352d4e82b33e237113ff4f085d756f174
bachdev <bachbom27@gmail.com>
2025-11-15 13:43:03 +0700

    update

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "",
+  assetPrefix: "./",
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: ["images.pexels.com", "www.pexels.com"],
   },
 };
